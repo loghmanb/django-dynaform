946d3e52e0ee297f8906700785da34d0cb41823f
Loghman Barari <loghmanb@gmail.com>
2023-09-23 08:35:01 +0000

    remove example

--- example/sample_form/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SampleFormConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "sample_form"
--- example/sample_form/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from dynaform.service import DynaFormContext
-
-def home(request):
-    context = {
-        'dynaform': DynaFormContext()
-    }
-    return render(request, "home.html", context=context)
