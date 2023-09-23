d51c1eaa26b8b6534c07282b5a2c4e4f461e1b3a
Loghman Barari <loghmanb@gmail.com>
2023-09-23 18:34:06 +0000

    update project.urls

--- example/django_dynaform_example/urls.py
+++ example/django_dynaform_example/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+    path('', include('dynaform.urls')),
     path('', include('sample_form.urls')),
     path("admin/", admin.site.urls),
 ]
