aff9cd2afec2ab1fcbf52274435a8980ccfe5e25
Loghman Barari <loghmanb@gmail.com>
2023-09-23 11:20:05 +0000

    add pylint

--- example/sample_form/views.py
+++ example/sample_form/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from dynaform.service import DynaFormContext
 
-def home(request):
+
+def home(request) -> HttpResponse:
     context = {
         'dynaform': DynaFormContext()
     }
