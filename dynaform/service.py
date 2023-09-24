57a2b166d0b47397e352cdad3d4ef130075b1f10
Loghman Barari <loghmanb@gmail.com>
2023-09-24 12:33:28 +0000

    fix pylint for ChoiceFieldValue

--- dynaform/service.py
+++ dynaform/service.py
@@ -30,8 +30,8 @@ def get_dynaform_and_data(dynaform_name: str) -> Union[None, List[Dict[str, Any]
 class ChoiceFieldValue(str):
     """Choice field value class."""
 
-    _value = None
-    _choices = {}
+    _value: Optional[Union[str, int]] = None
+    _choices: Dict = {}
 
     def init(self, value: Optional[Union[str, int]], choices: Dict) -> None:
         self._value = value
